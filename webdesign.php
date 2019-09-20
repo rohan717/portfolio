889fa3062bf240801e532cf377d72062ee324000
rohansawant <rohansawant717@gmail.com>
2019-09-20 12:40:50 +0530

    Initial Commit

---
+++ webdesign.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" title="no title" charset="utf-8"/>
+<link rel='stylesheet prefetch' href='https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'>
+<link rel="stylesheet" href="css/owl.carousel.min.css">
+<link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <title>Web Design and Development</title>
+  </head>
+  <body>
+  	<div id="about-banner">
+  	<?php 
+  	include("common/navbar.php");
+  	 ?>
+  	 <div class="container">
+  			<div class="row">
+					<div class="col-sm-6">
+						<div id="breadCumbHeader">
+							<h1> About our Company</h1>
+							<p>
+								<a href="">Home</a> / Web design and Developemnt
+							</p>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<img src="image/banner-img.png" class="img-fluid" alt="" />
+					</div>
+				</div>
+				</div> 
+  </div>	
+
+
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				
+			</div>
+		</div>
+	</div>
+</section>
